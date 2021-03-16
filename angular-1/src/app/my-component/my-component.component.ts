6ada4ada42ad97e42152e12d876a8502e9f20774
Marlon Giovanni Martinez Herrera <17041419@itdurango.edu.mx>
2021-03-16 14:55:21 -0600

    pass props

---
+++ angular-1/src/app/my-component/my-component.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css'],
+})
+export class MyComponentComponent implements OnInit {
+  @Input() author: string = '';
+  constructor() {}
+
+  ngOnInit() {}
+}
